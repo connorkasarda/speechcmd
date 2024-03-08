6843cb3abbe2872d9f9acb2795e4bebd4c57c390
Connor <connorkasarda@gmail.com>
2024-03-08 12:06:27 -0700

    First log. Researched packages.

---
+++ speechcmd/main.py
@@ -0,0 +1,13 @@
+"""
+file: main.py
+desc: entry point for program
+author: Connor Kasarda
+date: 2024-03-08
+"""
+
+def main():
+    print("Hello, world!")
+
+# Main function only called when script executed directly
+if __name__ == "__main__":
+    main()
